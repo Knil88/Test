681481b5d53cb3c2f6899b35ad37d5a86ecc7a95
Narducci Yari <narducciyari@gmail.com>
2023-11-06 21:23:53 +0100

    First commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import MyComponent from './components/MyComponents.vue'
+</script>
+
+<template>
+  <!-- <div>
+    <a href="https://vitejs.dev" target="_blank">
+      <img src="/vite.svg" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://vuejs.org/" target="_blank">
+      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+    </a>
+  </div>
+  <HelloWorld msg="Vite + Vue" /> -->
+  <MyComponent></MyComponent>
+</template>
+
+<style scoped>
+
+</style>
